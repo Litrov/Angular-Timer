052f847327d780d8ceaeecbdfe1880a846ed9ef3
Litrov <rafgih4552@gmail.com>
2022-06-17 16:12:09 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-timer';
+
+  ms: any = '0' + 0;
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+  hr: any = '0' + 0;
+
+  startTimer: any;
+  running = false;
+
+  start(): void {
+    if (!this.running) {
+      this.running = true;
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+        if (this.ms === 100) {
+          this.sec++;
+          this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+          this.ms = '0' + 0;
+        }
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+
+        if (this.min === 60) {
+          this.hr++;
+          this.hr = this.hr < 10 ? '0' + this.hr : this.hr;
+          this.min = '0' + 0;
+        }
+      }, 10);
+    } else {
+      this.stop();
+    }
+  }
+
+  stop(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+  }
+
+  reset(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+    this.hr = this.min = this.sec = this.ms = '0' + 0;
+  }
 }
